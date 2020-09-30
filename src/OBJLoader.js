cfb38feed135824ddf7a1a5be1096b2d7322b946
Matt Brucker <mpbrucker@gmail.com>
2020-09-30 18:39:04 -0500

    Adding face -> vertex parsing code

--- src/OBJLoader.js
+++ src/OBJLoader.js
@@ -15,12 +15,21 @@ function loadOBJ(filename) {
                     vertices.push([parseFloat(line[1]), parseFloat(line[2]), parseFloat(line[3])])
                 }
 
-                if (line[0] === 'f') {
-
-
-                    for (let i=2; i<line.length-2; i++) {
-                        
+                if (line[0] === 'f') { // If we have a face, parse the correct vertices
+                    let parsedVerts = [];
+                    for (let i=1; i< line.length; i++) { // First, we remove textures and normals... for now
+                        parsedVerts.push(line[i].split('/')[0])
                     }
+                    // triangle-ification method adapted from https://stackoverflow.com/questions/23723993/converting-quadriladerals-in-an-obj-file-into-triangles
+                    for (let i=1; i<parsedVerts.length-1; i++) { // Next, we break the list of vertices into triangles
+                        triangles.push(vertices[parsedVerts[0]-1]); 
+                        triangles.push(1.0);
+                        triangles.push(vertices[parsedVerts[i]-1]);
+                        triangles.push(1.0);
+                        triangles.push(vertices[parsedVerts[i+1]-1]);
+                        triangles.push(1.0);
+                    }
+                    console.log(triangles.flat())
                 }
             }
         })
